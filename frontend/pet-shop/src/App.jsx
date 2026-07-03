61635ae4c05cb26b29f31fce1cef0089b164eda4
sonalkumari-27 <sonalkumari2701@gmail.com>
2026-07-03 08:24:58 +0530

    my local changes saved

--- frontend/pet-shop/src/App.jsx
+++ frontend/pet-shop/src/App.jsx
@@ -1,17 +1,17 @@
 import "./App.css";
-import { useFormik } from "formik";
-import GroomingForm from "./components/forms/GroomingForm";
-import VaccinationForm from "./components/forms/VaccinationForm";
-import TransactionForm from "./components/forms/TransactionForm";
-import FoodForm from "./components/forms/FoodForm";
+import  "formik";
+//import GroomingForm from "./components/forms/GroomingForm";
+//import VaccinationForm from "./components/forms/VaccinationForm";
+//import TransactionForm from "./components/forms/TransactionForm";
+//import FoodForm from "./components/forms/FoodForm";
 import { BrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/auth/login";
-import RegisterPage from "./pages/auth/Register";
+//import RegisterPage from "./pages/auth/Register";
 
 function App() {
 	return (
 		<BrowserRouter>
-			<RegisterPage />
+			<LoginPage />
 		</BrowserRouter>
 	);
 }
