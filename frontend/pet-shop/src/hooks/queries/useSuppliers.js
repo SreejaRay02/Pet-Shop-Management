64bf253776bb51a434b2847861f9153e3a8687c1
sonalkumari-27 <sonalkumari2701@gmail.com>
2026-07-05 11:19:52 +0530

    changes in endpoints

--- frontend/pet-shop/src/hooks/queries/useSuppliers.js
+++ frontend/pet-shop/src/hooks/queries/useSuppliers.js
@@ -9,3 +9,19 @@ export const useSuppliers = (params) => {
     queryFn: () => supplierService.getAll(params).then((r) => r.data),
   });
 };
+
+export const useSupplierById = (id) => {
+  return useQuery({
+    queryKey: [...SUPPLIERS_KEY, id],
+    queryFn: () => supplierService.getById(id).then((r) => r.data),
+    enabled: !!id,
+  });
+};
+
+export const useSupplierByEmail = (email) => {
+  return useQuery({
+    queryKey: [...SUPPLIERS_KEY, 'email', email],
+    queryFn: () => supplierService.getByEmail(email).then((r) => r.data[0]),
+    enabled: !!email,
+  });
+};
